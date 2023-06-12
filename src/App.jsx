39657865851e423b6cb18b484bc145c4c70eb32e
jenDevelopez <jen1992lo@gmail.com>
2023-06-12 13:00:29 +0200

    Repositorio inicializado

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Escena } from './components/Escena'
+
+function App() {
+
+  return (
+    <>
+      <Escena />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Escena.jsx
@@ -0,0 +1,5 @@
+export function Escena() {
+  return(
+    <p>Nuestro héroe estaba flotando en el espacio lateral cuando a lo lejos vidrió una nave espacial</p>
+  )
+}
